10eb2d4a04700830044767c41fccc951228f6f58
savorywatt <savorywatt@ross-dev.(none)>
2014-04-15 00:02:37 -0500

    Add openscad builder. Rough in coffeescad builder

---
+++ tailorscad/builder/openscad.py
@@ -0,0 +1,35 @@
+import subprocess
+
+from tailorscad.config import ScadConfig
+
+
+BASE_DIR = '/usr/bin/'
+DEFAULT = BASE_DIR + 'openscad'
+
+
+def build_with_openscad(state):
+
+    args = build_args_from_state(state)
+
+    try:
+        subprocess.check_call(args, state.working_dir)
+        return True
+    except subprocess.CalledProcessError as (e):
+        print str(e)
+        return False
+
+
+def build_args_from_state(state):
+
+    #executable = ScadConfig.open_scad if ScadConfig.open_scad else DEFAULT
+    executable = 'openscad'
+
+    print state.params
+    replace = [':'.join((key, str(value)))
+               for key, value in state.params.iteritems()]
+
+    args = [executable, '-o', state.output_directory, '-D'] + replace
+
+    args.append(state.main_file)
+
+    return args

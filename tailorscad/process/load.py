8c203e184b6579260462a4efbafe6255c0a4983b
savorywatt <savorywatt@ross-dev.(none)>
2014-04-15 00:05:47 -0500

    Stub out load for the state

---
+++ tailorscad/process/load.py
@@ -0,0 +1,11 @@
+
+
+def load_state_files(state):
+
+
+    # check if we're loading from git first
+
+    # check if the directory is readable
+
+    # check if the write out directory is writeable
+    pass

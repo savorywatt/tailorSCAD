dd0f4d180217bb0ce8b2b0c2cd28c1247b578963
savorywatt <savorywatt@ross-dev.(none)>
2014-04-19 23:44:01 -0500

    Fix up config parser based on testing

--- tailorscad/config/config.py
+++ tailorscad/config/config.py
@@ -1,7 +1,8 @@
 import json
-import logging
+import os
 
 from tailorscad.state import State
+from tailorscad.config import ConfigData
 
 
 def parse_config_arg(parsed_command_line_args):
@@ -16,15 +17,16 @@ def _parse_config_file(config_file):
     """Private function that takes a file object and parses it using the json
     library. Helps with testing reading files from the command line."""
 
-    loaded_json = None
+    loaded_json = {}
     try:
         loaded_json = json.load(config_file)
-    except:
-        logging.info("Unable to parse config file")
+    except Exception as e:
+        print str(e)
+        print 'can\'t load config!'
 
     loaded_json['states'] = _parse_states(loaded_json)
 
-    return loaded_json
+    return ConfigData(loaded_json)
 
 
 # TODO: Make this obey DRY
@@ -33,16 +35,27 @@ def _parse_states(loaded_json):
     file is in the final position of the state list.
     """
 
+    if not loaded_json:
+        print 'no json loaded cannot parse states'
+        return
+
+    for key, value in loaded_json.iteritems():
+        print ':'.join((key, str(value)))
+
     states = []
     global_params = loaded_json.get('global_params')
-    require_params = loaded_json.get('required_params')
+    require_params = loaded_json.get('require_params')
     requires = loaded_json.get('requires')
+
+    _build_directories(loaded_json)
 
     for key, state in requires.iteritems():
 
             state['output_directory'] = loaded_json.get('output_directory')
             state['working_directory'] = loaded_json.get('working_directory')
-            state['params'] = require_params.get(key) + global_params
+
+            state_params = require_params.get(key, {})
+            state['params'] = state_params.update(global_params)
 
             states.append(_parse_state(key, state))
 
@@ -55,6 +68,17 @@ def _parse_states(loaded_json):
     states.append(_parse_state('main', main_state))
 
     return states
+
+
+def _build_directories(loaded_json):
+    """Ensures that we always have a directory to work with if they are not
+    defined in the configuration file."""
+
+    update = {}
+    update['output_directory'] = loaded_json.get(
+        'output_directory', os.path.join(os.getcwd(), 'bin'))
+    update['working_directory'] = loaded_json.get(
+        'working_directory', os.getcwd())
 
 
 def _parse_state(name, state):
